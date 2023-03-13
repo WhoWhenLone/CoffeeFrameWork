69bea657bd921819adb9bc19897a5f1606b6a7cd
nancheng <nancheng@im30.lan>
2023-03-13 12:00:12 +0800

    [add] 添加编辑器扩展测试代码

---
+++ Assets/Editor/EditorTools.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+namespace CoffeeEditor
+{
+    public class EditorTools
+    {
+        // [MenuItem("Assets")]
+    }
+}
---
+++ Assets/Scripts/Test/Bullet.cs
@@ -0,0 +1,17 @@
+﻿// File: Bullet.cs
+// Create by nancheng
+// DateTime: ${DATE}
+// Desc: XXX
+// DateTime: ${DATE} ${TIME}
+
+using UnityEngine;
+using CoffeeFrameWork;
+
+namespace CoffeeFrameWork
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField]
+        public TestScriptableObject data;
+    }
+}
