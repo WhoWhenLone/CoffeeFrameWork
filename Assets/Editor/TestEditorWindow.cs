07943e3ed1f2fa8bb015cf74256d2d63b5fd7c74
nancheng <2878375578@qq.com>
2023-09-18 16:31:22 +0800

    【开发】测试fork客户端提交

--- Assets/Editor/TestEditorWindow.cs
+++ Assets/Editor/TestEditorWindow.cs
@@ -1,5 +1,4 @@
-﻿using Sirenix.OdinInspector;
-using Unity.VisualScripting;
+﻿using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,35 +10,40 @@ namespace CoffeeEditor
         private static void ShowWindow()
         {
             var window = GetWindow<TestEditorWindow>();
-            window.titleContent = new GUIContent("TestEditorWindow");
-            window.Show();
+            if (window != null)
+            {
+                window.titleContent = new GUIContent("TestEditorWindow");
+                window.Show();
+            }
         }
 
         private void OnGUI()
         {
-            GUILayout.BeginVertical();
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("测试水平排列");
-            if(GUILayout.Button("测试按钮1"))
-            {
-                EditorUtility.DisplayDialog("title", "测试按钮1", "OK");
-            }
-            
-            if(GUILayout.Button("测试按钮2"))
-            {
-                EditorUtility.DisplayDialog("title", "测试按钮2", "OK");
-            }
-            
-            GUILayout.EndHorizontal();
-            
-            GUILayout.Label("选择Prefab资源");
-            GameObject go;
-            GUILayout.EndVertical();
+            // GUILayout.BeginVertical();
+            // GUILayout.BeginHorizontal();
+            // GUILayout.Label("测试水平排列");
+            // if(GUILayout.Button("测试按钮1"))
+            // {
+            //     EditorUtility.DisplayDialog("title", "测试按钮1", "OK");
+            // }
+            //
+            // if(GUILayout.Button("测试按钮2"))
+            // {
+            //     EditorUtility.DisplayDialog("title", "测试按钮2", "OK");
+            // }
+            //
+            // GUILayout.EndHorizontal();
+            //
+            // GUILayout.Label("选择Prefab资源");
+            // GameObject go;
+            // GUILayout.EndVertical();
+            GUILayout.Label("窗口");
         }
 
         private void OnClick1()
         {
             
         }
+        
     }
 }
---
+++ Assets/Scripts/Engine/Assets/AssetManager.cs
@@ -0,0 +1,33 @@
+﻿// File:    AssetManager.cs
+// Author:  nancheng
+// Date:    2023-6-5 14:23:45
+// Desc:    资源管理
+
+using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AssetManager
+{
+
+    public static void LoadAsset<T>(string address, Action callback)
+    {
+        
+    }
+
+    public static void InstantiateAsset<T>(string address, Action callback)
+    {
+
+        
+    }
+
+    public static void ReleaseAsset(string address, Action callback)
+    {
+        
+    }
+
+    public static void ReleaseInstantiate(string address, Action callback)
+    {
+        
+    }
+}
